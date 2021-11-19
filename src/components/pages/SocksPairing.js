eeed85f8c243e45185a2db1058cbaa6ace7ee678
Matthieu <matthieu.bezard92@gmail.com>
2021-11-19 18:35:01 +0100

    Sock svg editable

--- src/components/pages/SocksPairing.js
+++ src/components/pages/SocksPairing.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Sock from "../components/Sock";
 
 export default function SocksPairing() {
-    return <div>Pairing</div>
+    return <div className={"h-screen flex content-center bg-greyLight-1"}>
+        <div className={"m-auto shadow flex flex-col content-center p-5"}>
+            <div>
+                <div>Choose socks pool</div>
+                <div>
+                    <Sock patternColor={"red"} color={"blue"} pattern={"None"} lineAmount={0} height={150} width={150}/>
+                    <Sock patternColor={"red"} color={"blue"} pattern={"None"} lineAmount={1} height={150} width={150}/>
+                    <Sock patternColor={"red"} color={"blue"} pattern={"None"} lineAmount={2} height={150} width={150}/>
+                    <Sock/>
+                </div>
+            </div>
+            <div>Choose Algorithm</div>
+        </div>
+    </div>
 }
